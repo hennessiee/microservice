58d88c70fa4a1e37fc3ba4c339227940ca4cade7
ft2FRS-p4MPaxyB2ihiB <i477574@fontysict.nl>
2024-10-30 23:27:46 +0100

    added test configurations for services

--- facility-service/src/test/java/micro/facilityservice/FacilityServiceApplicationTests.java
+++ facility-service/src/test/java/micro/facilityservice/FacilityServiceApplicationTests.java
@@ -2,8 +2,10 @@ package micro.facilityservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FacilityServiceApplicationTests {
 
     @Test
