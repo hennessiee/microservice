2ed4e85f717a37be77f24a1d9c9c6ad3d5b0525e
ft2FRS-p4MPaxyB2ihiB <i477574@fontysict.nl>
2024-10-30 23:21:08 +0100

    added pipeline

--- account-service/src/test/java/micro/account/accountservice/AccountServiceApplicationTests.java
+++ account-service/src/test/java/micro/account/accountservice/AccountServiceApplicationTests.java
@@ -2,9 +2,13 @@ package micro.account.accountservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AccountServiceApplicationTests {
+
+
 
 	@Test
 	void contextLoads() {
