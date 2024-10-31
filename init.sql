3d246152c4d8e1be36b0f0102158ddde55511d6b
Nathaniel Ravindra <i477574@fhict.nl>
2024-10-31 15:26:48 +0100

    modified also init.sql to ensure success of sql startup

--- init.sql
+++ init.sql
@@ -1,4 +1,9 @@
 CREATE DATABASE IF NOT EXISTS micro;
+
+-- Drop the user if it exists
+DROP USER IF EXISTS 'root'@'%';
+
+-- Create the user and grant privileges
 CREATE USER 'root'@'%' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
