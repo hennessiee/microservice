bda30141f1cb43b71c19288f4f00bbf1b0be122b
ravindra.nathaniel@gmail.com <i477574@fhict.nl>
2024-12-05 10:37:53 +0100

    updated register controller to return 201 instead of 200 when created

--- account-service/src/main/java/micro/account/accountservice/controller/RegisterController.java
+++ account-service/src/main/java/micro/account/accountservice/controller/RegisterController.java
@@ -8,6 +8,8 @@ import micro.account.accountservice.domain.register.RegisterResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin(origins="http://localhost:5173",allowedHeaders="*")
@@ -17,8 +19,9 @@ public class RegisterController {
     @PostMapping("/register")
     public ResponseEntity<RegisterResponse> register(@RequestBody @Valid RegisterRequest registerRequest){
         RegisterResponse response =registerUseCase.register(registerRequest);
+        URI location = URI.create("/register/" + "user"); // Replace with the correct URI structure
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.created(location).body(response);
     }
 
 }
