85516ea33b13cfb6d71213087907a8fd18d6da02
ravindra.nathaniel@gmail.com <i477574@fhict.nl>
2024-12-05 10:38:03 +0100

    updated facility controller to return 201 instead of 200 when created

--- facility-service/src/main/java/micro/facilityservice/controller/FacilityController.java
+++ facility-service/src/main/java/micro/facilityservice/controller/FacilityController.java
@@ -8,6 +8,8 @@ import micro.facilityservice.domain.facility.CreateFacilityResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin(origins="http://localhost:5173",allowedHeaders="*")
@@ -18,7 +20,8 @@ private final CreateFacilityUseCase createFacilityUseCase;
     @PostMapping("/create")
     public ResponseEntity<CreateFacilityResponse> register(@RequestBody @Valid CreateFacilityRequest createFacilityRequest){
         CreateFacilityResponse response =createFacilityUseCase.createFacility(createFacilityRequest);
+        URI location = URI.create("/facility/" + response.getId()); // Replace with the correct URI structure
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.created(location).body(response);
     }
 }
