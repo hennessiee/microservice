39581133043baaf99fb8f65d8c616e26be0164da
hennessiee <89709449+hennessiee@users.noreply.github.com>
2025-01-08 21:59:23 +0100

    Update test-script.js
    changed options to ramp up to 100 instead of constant 100 users

--- test-script.js
+++ test-script.js
@@ -1,9 +1,10 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-export let options = {
-  vus: 100, 
-  duration: '1m', 
+export const options = {
+  stages: [
+    { duration: '1m', target: 100 }, 
+  ],
 };
 
 export default function () {
