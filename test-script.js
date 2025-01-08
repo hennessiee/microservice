3c9e0ee5262ff0f6b007f6a91a5245c393aaad6e
Nathaniel_PC <ravindra.nathaniel@gmail.com>
2025-01-08 22:15:53 +0100

    UPDATE: changed test-scripts to test with maximum of 100 users

--- test-script.js
+++ test-script.js
@@ -1,43 +1,42 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-export let options = {
-  vus: 5, // Virtual Users
-  duration: '1m', // Test duration
+export const options = {
+  stages: [
+    { duration: '1m', target: 100 }, 
+  ],
 };
 
 export default function () {
   // Define the base URL from an environment variable, with a default fallback
   let baseUrl = __ENV.BASE_URL || 'http://192.168.141.6:30080';
 
-  // 1. GET Request - Stock Group
-  let res1 = http.get(`${baseUrl}/inventory-service/stockgroup/1`);
-  check(res1, {
-    'Stock Group - status is 200': (r) => r.status === 200,
-  });
+  // // 1. GET Request - Stock Group
+  // let res1 = http.get(`${baseUrl}/inventory-service/stockgroup/1`);
+  // check(res1, {
+  //   'Stock Group - status is 200': (r) => r.status === 200,
+  // });
 
-  // 2. POST Request - Login
-  let loginPayload = JSON.stringify({
-    username: 'johndoe',
-    password: '1234',
-  });
-  let loginHeaders = { 'Content-Type': 'application/json' };
-  let res2 = http.post(
-    `${baseUrl}/account-service/auth/login`,
-    loginPayload,
-    { headers: loginHeaders }
-  );
-  check(res2, {
-    'Login - status is 200': (r) => r.status === 200,
-  });
+  // // 2. POST Request - Login
+  // let loginPayload = JSON.stringify({
+  //   username: 'johndoe',
+  //   password: '1234',
+  // });
+  // let loginHeaders = { 'Content-Type': 'application/json' };
+  // let res2 = http.post(
+  //   `${baseUrl}/account-service/auth/login`,
+  //   loginPayload,
+  //   { headers: loginHeaders }
+  // );
+  // check(res2, {
+  //   'Login - status is 200': (r) => r.status === 200,
+  // });
 
-  // 3. GET Request - Stock
-  let res3 = http.get(`${baseUrl}/inventory-service/stock/1`);
-  check(res3, {
-    'Stock - status is 200': (r) => r.status === 200,
-  });
-
-  // 4. POST Request - Create Stock
+  // // 3. GET Request - Stock
+  // let res3 = http.get(`${baseUrl}/inventory-service/stock/1`);
+  // check(res3, {
+  //   'Stock - status is 200': (r) => r.status === 200,
+  // });
   let createPayload = JSON.stringify({
     name: 'Example Item',
     description: 'This is an example item for stock management',
