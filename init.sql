df3484a047590cc13c0cf87229fddfb458ef7afd
Nathaniel_PC <ravindra.nathaniel@gmail.com>
2025-01-17 19:19:20 +0100

    updated init.sql to confirm persistence

--- init.sql
+++ init.sql
@@ -1,9 +1,6 @@
 CREATE DATABASE IF NOT EXISTS micro;
 
--- Drop the user if it exists
-DROP USER IF EXISTS 'root'@'%';
-
--- Create the user and grant privileges
-CREATE USER 'root'@'%' IDENTIFIED BY 'root';
+-- Check if the user already exists before creating it
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
